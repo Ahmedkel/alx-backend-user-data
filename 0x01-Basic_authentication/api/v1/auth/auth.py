124c89c79654f7ecc00d00457ca0068bed837e6b
Ahmedkel <ahmedkel@gmail.com>
2024-02-12 16:27:13 +0100

    implement  routes which don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,13 +7,23 @@ from typing import List, TypeVar
 class Auth:
     """Authentication class."""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to check if auth is required."""
-        return False
+        """ Method to check if auth is required. """
+        if path is None:
+            return True
+
+        if excluded_paths is None or not excluded_paths:
+            return True
+
+        for excluded_path in excluded_paths:
+            if path == excluded_path or excluded_path.startswith(path):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """ Method to get authorization header."""
+        """ Method to get authorization header. """
         return None
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ Method to get user from request."""
+    def current_user(self, request=None) -> TypeVar('User'): # type: ignore
+        """ Method to get user from request. """
         return None
