71be130ab4fd732f365927fbdc2b1017b198daf1
Ahmedkel <ahmedkel@gmail.com>
2024-02-15 15:56:14 +0100

    more fixing

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -3,9 +3,7 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.auth.auth import Auth
 from api.v1.views import app_views
-from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -14,7 +12,6 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
 AUTH_TYPE = os.getenv("AUTH_TYPE")
 if AUTH_TYPE == "auth":
@@ -36,7 +33,9 @@ elif AUTH_TYPE == "session_db_auth":
 
 @app.before_request
 def bef_req():
-    """ Filter each request. """
+    """
+    Filter each request before it's handled by the proper route
+    """
     if auth is None:
         pass
     else:
@@ -64,14 +63,14 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler.
+    """ Request unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler.
+    """ Request unauthorized handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
